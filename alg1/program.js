29a8f30d30392646116fcfcbf25fd32a37c49890
lirasusejdev <lis.amaral@grupoboticario.com.br>
2022-01-04 08:58:37 -0300

    add files

---
+++ alg1/program.js
@@ -0,0 +1,16 @@
+// import project - dependencies
+let teclado = require('prompt-sync')()
+
+//Entrada de dados
+console.log('Digite o primeiro valor')
+let valor1 = teclado()
+let valor2 = teclado()
+
+//Processamento
+let soma = valor1 + valor2
+
+//Saida de dados
+//Voce digitou o valor X
+//console.log('Voce digitou o valor', valor2)
+//console.log(`Voce digitou o valor ${valor1}`)
+console.log(`Resultado: ${soma}`)
